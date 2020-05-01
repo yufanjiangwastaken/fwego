8c3995fbb63508e15d7cabde99625444aea09ebd
Ben Hansen <bnhansn@gmail.com>
2020-04-30 18:26:14 -0700

    fix exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
 
@@ -21,13 +21,5 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    commonjs()
-  ]
+  plugins: [peerDepsExternal(), resolve(), typescript(), commonjs()]
 }
